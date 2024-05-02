c4ef96f6709ff8f1e460a7d731edd4e5b0d593ba
Aadesh Singh <136155607+aadi6190@users.noreply.github.com>
2024-05-03 01:44:44 +0530

    Add files via upload

---
+++ jspractise/script.js
@@ -0,0 +1,162 @@
+// alert("Welcome My frnds Welcome");
+// let Aadi = 69;
+// console.log(Aadi);
+// console.log(Aadi);
+// console.log(Aadi);
+// console.log(Aadi);
+
+
+
+
+// let a = 10;
+// let b = 10;
+
+// let c = 10;
+// let d = 10;
+// console.log(a + b > 10);
+
+// let firstName = 'Aadi';
+// let age = 20;
+// console.log(`my name is ${firstName} and I am ${age} years old`);
+
+
+
+// let myAge =  21;
+// let herAge = 17;
+// if ( myAge>=21 && herAge>=18 ){
+//     console.log("Allowed to marry her");
+// }
+// else{
+//     console.log("bhak bsdk phle  bada hoja");
+// }
+
+
+// let x=20;
+// let y = 25;
+// if(x>=10 && y>=20){
+//     alert("The value is greather than 10=");
+// }
+// else{
+//     alert("ma chuda value kam h 10 se = ")
+// }
+
+
+// == , ===
+// console.log(12 == 12);
+// console.log('12' == 12);
+// console.log(12 === 12);
+// console.log('12' === 12);
+// console.log([69] === [12]);
+
+
+// for off loop
+
+// let str = "Aadesh singh";
+// let size = 0;
+
+// for(let i of str){
+//     console.log("i",i);
+//     size++;
+// }
+// console.log("the size is= ", size);
+
+
+
+//print all the even number till 100
+
+// let sum = 0;
+// for (let i = 0; i <= 100; i++) {
+//   sum += i;
+// }
+// console.log("The sum =", sum);
+
+// game
+ 
+
+// let gameNum = 20;
+
+// let UserGuss =  prompt("guess the num:");
+// console.log(UserGuss);
+
+// while(UserGuss != gameNum){
+//     prompt("U put the glt num, enter the value again :");
+// }
+// console.log("congo u entered right num");
+
+
+// Define the array
+// Define the array
+// let arr = [1, 2, 3, 4, 3, 5, 9];
+
+
+// // Access the div where the array will be printed
+// let arrContentDiv = document.getElementById("arrayContent");
+
+// arrContentDiv.textContent = arr;
+
+
+
+
+//Array
+// let Strings = [
+//   "Hello",
+//   "How are you?",
+//   "I'm fine, thank you!",
+//   "What's your name?",
+//   "Nice to meet you"
+// ];
+
+//   for (let i=0; i<=Strings.length; i++){
+// console.log(Strings[i])
+//   }
+
+
+// avarage of number in array
+
+// let marks = [85,97,44,37,76,60];
+// let sum = 0
+
+// for (let i=0; i<marks.length; i++ ){
+
+//  sum += marks[i];
+
+// }
+//  let avarage = sum / marks.length;
+//  console.log(avarage);
+
+//using for off loop
+// for ( let val of marks){
+//   sum += val;
+// }
+ 
+// let avarage = sum / marks.length ;
+// console.log("The Avrage Marks is =",  avarage);
+
+
+ 
+// for (let val of price){
+// let offer = val/10;
+// price[i] -= offer;
+// console.log(price[i]);
+
+// i++
+// }
+
+// for (let i=0;i <price.length; i++){
+//   let offer = price[i] / 10;
+//   price[i] -= offer;
+// }
+// console.log(price);
+
+
+
+// merge two array
+
+let array1 = ["apple", "banana", "orange", "grape", "kiwi"];
+
+
+let array2 = ["cat", "dog", "rabbit", "hamster", "bird"];
+
+
+let newArr = array1.concat(array2);
+console.log(newArr);
