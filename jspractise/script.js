08f43af53f5cb9b57d92232f11f3b89077bf84d5
Aadesh Singh <136155607+aadi6190@users.noreply.github.com>
2024-05-08 21:45:48 +0530

    Update script.js

--- jspractise/script.js
+++ jspractise/script.js
@@ -1,162 +1,89 @@
-// alert("Welcome My frnds Welcome");
-// let Aadi = 69;
-// console.log(Aadi);
-// console.log(Aadi);
-// console.log(Aadi);
-// console.log(Aadi);
+function greet() {
+    alert("Welcome My friends Welcome!");
+}
+
+greet();
+
+let aadi = 69;
+let baba = "Hello, world!";
+let c = true;
+let d = [1, 2, 3];
+let e = { name: "John", age: 25 };
+
+console.log(aadi);
+console.log(baba);
+console.log(c);
+console.log(d);
+console.log(e);
+
+\
+let myAge = 21;
+let herAge = 17;
+
+if (myAge >= 21 && herAge >= 18) {
+    console.log("Allowed to marry her");
+} else if (myAge >= 21 && herAge < 18) {
+    console.log("She's too young to marry");
+} else {
+    console.log("Wait until you're 21 to consider marriage");
+}
 
 
+// Nested loops
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= i; j++) {
+        console.log(j);
+    }
+}
+
+// While loop
+let i = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+// Do-while loop
+let j = 0;
+do {
+    console.log(j);
+    j++;
+} while (j < 5);
 
 
-// let a = 10;
-// let b = 10;
+let marks = [85, 97, 44, 37, 76, 60];
 
-// let c = 10;
-// let d = 10;
-// console.log(a + b > 10);
+// Using reduce to calculate sum
+let sum = marks.reduce((acc, curr) => acc + curr, 0);
 
-// let firstName = 'Aadi';
-// let age = 20;
-// console.log(`my name is ${firstName} and I am ${age} years old`);
+let average = sum / marks.length;
+console.log("Average marks:", average);
 
+// Multidimensional array
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
 
-
-// let myAge =  21;
-// let herAge = 17;
-// if ( myAge>=21 && herAge>=18 ){
-//     console.log("Allowed to marry her");
-// }
-// else{
-//     console.log("bhak bsdk phle  bada hoja");
-// }
+// Flatten the matrix
+let flattenedMatrix = matrix.flat();
+console.log("Flattened Matrix:", flattenedMatrix);
 
 
-// let x=20;
-// let y = 25;
-// if(x>=10 && y>=20){
-//     alert("The value is greather than 10=");
-// }
-// else{
-//     alert("ma chuda value kam h 10 se = ")
-// }
+// Function to calculate factorial
+function factorial(n) {
+    if (n === 0 || n === 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
 
+console.log("Factorial of 5:", factorial(5));
 
-// == , ===
-// console.log(12 == 12);
-// console.log('12' == 12);
-// console.log(12 === 12);
-// console.log('12' === 12);
-// console.log([69] === [12]);
+// Arrow function
+const greetUser = name => {
+    console.log(`Welcome, ${name}!`);
+}
 
-
-// for off loop
-
-// let str = "Aadesh singh";
-// let size = 0;
-
-// for(let i of str){
-//     console.log("i",i);
-//     size++;
-// }
-// console.log("the size is= ", size);
-
-
-
-//print all the even number till 100
-
-// let sum = 0;
-// for (let i = 0; i <= 100; i++) {
-//   sum += i;
-// }
-// console.log("The sum =", sum);
-
-// game
- 
-
-// let gameNum = 20;
-
-// let UserGuss =  prompt("guess the num:");
-// console.log(UserGuss);
-
-// while(UserGuss != gameNum){
-//     prompt("U put the glt num, enter the value again :");
-// }
-// console.log("congo u entered right num");
-
-
-// Define the array
-// Define the array
-// let arr = [1, 2, 3, 4, 3, 5, 9];
-
-
-// // Access the div where the array will be printed
-// let arrContentDiv = document.getElementById("arrayContent");
-
-// arrContentDiv.textContent = arr;
-
-
-
-
-//Array
-// let Strings = [
-//   "Hello",
-//   "How are you?",
-//   "I'm fine, thank you!",
-//   "What's your name?",
-//   "Nice to meet you"
-// ];
-
-//   for (let i=0; i<=Strings.length; i++){
-// console.log(Strings[i])
-//   }
-
-
-// avarage of number in array
-
-// let marks = [85,97,44,37,76,60];
-// let sum = 0
-
-// for (let i=0; i<marks.length; i++ ){
-
-//  sum += marks[i];
-
-// }
-//  let avarage = sum / marks.length;
-//  console.log(avarage);
-
-//using for off loop
-// for ( let val of marks){
-//   sum += val;
-// }
- 
-// let avarage = sum / marks.length ;
-// console.log("The Avrage Marks is =",  avarage);
-
-
- 
-// for (let val of price){
-// let offer = val/10;
-// price[i] -= offer;
-// console.log(price[i]);
-
-// i++
-// }
-
-// for (let i=0;i <price.length; i++){
-//   let offer = price[i] / 10;
-//   price[i] -= offer;
-// }
-// console.log(price);
-
-
-
-// merge two array
-
-let array1 = ["apple", "banana", "orange", "grape", "kiwi"];
-
-
-let array2 = ["cat", "dog", "rabbit", "hamster", "bird"];
-
-
-let newArr = array1.concat(array2);
-console.log(newArr);
+greetUser("Alice");
